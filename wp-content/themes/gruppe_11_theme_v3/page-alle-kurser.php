afa8d44f6da97bc80071773cb25990bacab8b856
emilie carlsen <emil103d@stud.kea.dk>
2021-11-03 15:23:10 +0100

    First commit

--- wp-content/themes/gruppe_11_theme_v3/page-alle-kurser.php
+++ wp-content/themes/gruppe_11_theme_v3/page-alle-kurser.php
@@ -16,33 +16,33 @@
 
 			<script> 
 console.log("Hip Hurra")
-// let temp = document.querySelector("template");
-// const liste = document.querySelector("#kurser-oversigt");
-// let kurser = []; 
-// // let posts;
-// const dbUrl = "https://www.amadeusnoah.dk/kea/semester_2/09_cms/g11_ungdomsbyen/wp-json/wp/v2/kursus";
-// async function getJson() {
-// 	const data = await fetch(dbUrl);
-// 	kurser = await data.json();
-// 	visKurser();
-// }
+let temp = document.querySelector("template");
+const liste = document.querySelector("#kurser-oversigt");
+let kurser = []; 
+// let posts;
+const dbUrl = "https://www.amadeusnoah.dk/kea/semester_2/09_cms/g11_ungdomsbyen/wp-json/wp/v2/kursus";
+async function getJson() {
+	const data = await fetch(dbUrl);
+	kurser = await data.json();
+	visKurser();
+}
 
-// function visKurser() {
-// 	console.log(kurser); 
-// 	liste.textContent= "";
-// 	kurser.forEach(kursus => {
-// 		const klon = temp.cloneNode(true).content;
-// 		klon.querySelector("h2").textContent = kursus.title.rendered;
-// 		klon.querySelector("img").src = kursus.billede.guid;
-// 		klon.querySelector("p").innerHTML = kursus.pris + " kr.";
-// 		klon.querySelector("article").addEventListener("click", () => {
-// 			location.href = kursus.link;
-// 		})
-// 		liste.appendChild(klon);
-// 	})
-// }
+function visKurser() {
+	console.log(kurser); 
+	liste.textContent= "";
+	kurser.forEach(kursus => {
+		const klon = temp.cloneNode(true).content;
+		klon.querySelector("h2").textContent = kursus.title.rendered;
+		klon.querySelector("img").src = kursus.billede.guid;
+		klon.querySelector("p").innerHTML = kursus.pris + " kr.";
+		klon.querySelector("article").addEventListener("click", () => {
+			location.href = kursus.link;
+		})
+		liste.appendChild(klon);
+	})
+}
 
-// getJson();
+getJson();
 
 
 			</script> 
